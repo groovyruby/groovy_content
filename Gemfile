cb18b899f1f91a098878b343887d59207fe0d214
Maciej Litwiniuk <maciej@galdomedia.pl>
2010-09-07 13:11:35 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'mysql2'
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'ruby-debug'
+
+
+
+group :test, :development do
+  gem "rspec-rails", ">= 2.0.0.beta.20"
+  gem "autotest"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GroovyContent::Application.config.secret_token = 'f9703457ca013a55666992e17669a162bc9c4a253704f2d957cd681da7a699288ad68bd2645fcf171bcf8a8c50466d1a713473c65d2b1ceccb78da9caac8faf3'

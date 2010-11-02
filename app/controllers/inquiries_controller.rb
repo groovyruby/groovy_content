b81dc4d9ca0a5a9c4ab75bfdc0786d1f369c708c
Maciej Litwiniuk <maciej@galdomedia.pl>
2010-11-02 12:41:35 +0100

    inquiries controller is not protected from forgery and responds depending to call (js or html)

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,4 +1,5 @@
 class InquiriesController < GroovyContentController
+  protect_from_forgery :except=>[:create]
   def new
     inquiry_form = current_site.inquiry_forms.find_by_slug!(params[:form])
     @inquiry = Inquiry.new
@@ -17,7 +18,11 @@ class InquiriesController < GroovyContentController
       if @inquiry.save
         InquiryMailer.inquiry_received(@inquiry).deliver
         # It's bad, I know...
-        render :action => "create"
+        
+        respond_to do |format|
+          format.html { return redirect_to(:back, :notice=>"Dziękujemy za przesłanie formularza") }
+          format.js  { render :action => "create" }
+        end
       else
         render :action => "new" 
       end

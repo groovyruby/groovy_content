2d1b73a8d6d7a1b69f2c000d071fee26f1e7962c
Maciej Litwiniuk <maciej@galdomedia.pl>
2010-11-09 14:56:32 +0100

    admin required in admin area ;)

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,11 +1,17 @@
 class AdminController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :require_admin
   before_filter :get_sites
+  
 
   layout "admin"
 
 
-
+  private
+  
+  def require_admin
+    redirect_to(root_url) unless current_user.is_admin?
+  end
   def get_sites
     @sites = Site.all
   end

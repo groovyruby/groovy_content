a242a0020b6f5beac604beab5ba613599bd07499
Maciej Litwiniuk <maciej@galdomedia.pl>
2010-12-29 11:02:07 +0100

    property_types propagate

--- app/models/property_type.rb
+++ app/models/property_type.rb
@@ -10,11 +10,27 @@ class PropertyType < ActiveRecord::Base
   validates :field_type, :presence=>true, :inclusion=>{:in=>PropertyType::FIELD_TYPES}
   
   attr_accessible :name, :identifier, :field_type, :params
-
+  
   before_validation :fill_in_identifier
+  
+  after_create :propagate_field
 
   def fill_in_identifier
     self.identifier = self.name.parameterize if self.identifier.blank? and not self.name.blank?
+  end
+  
+  def propagate_field
+    unless self.page_type.blank?
+      unless self.page_type.pages.empty?
+        for p in self.page_type.pages
+          prop = self.properties.new
+          prop.page = p
+          prop.page_type = self.page_type
+          prop.save
+          p.properties << prop
+        end
+      end
+    end
   end
 
 end
